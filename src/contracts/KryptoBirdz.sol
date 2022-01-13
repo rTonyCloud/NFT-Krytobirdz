639a2c173899942965700d575d15415fd852b2e8
Tony Rivera <rtonycloud@gmail.com>
2022-01-13 16:33:30 -0500

    fixed error ;

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './ERC721Connector.sol'
+import './ERC721Connector.sol';
 
 contract Krytobird is ERC721Connector {
 
     constructor() ERC721Connector('KrytoBird', 'KBIRDZ') {
-        
+        {}
     }
     
 }
